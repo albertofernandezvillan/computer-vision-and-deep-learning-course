7ff5ff8c8582fb7c5159cc18f569ec62d8956d55
Alberto Fernández Villán <alberto.fernandez@fundacionctic.org>
2021-03-09 14:49:27 +0100

    Update hello_opencv.py

--- scripts/flask/hello_opencv.py
+++ scripts/flask/hello_opencv.py
@@ -16,6 +16,17 @@ run_with_ngrok(app)
 def home(): 
     return "Flask app server is running"
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
 @app.route('/canny', methods=['GET'])
 def canny_processing():
     # Get the image:
